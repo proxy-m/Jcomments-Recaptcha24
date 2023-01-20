b423f69adb0007a992932528627f90656a4165b3
Mikhail B <wproxym@at.gmail>
2023-01-20 14:55:07 +0300

    fix link

--- site/tpl/default/tpl_index.php
+++ site/tpl/default/tpl_index.php
@@ -75,7 +75,7 @@ jcomments.setList('comments-list');
 				}
 			}
 ?>
-<div id="comments-footer" align="center"><?php echo $this->getVar('support'); ?></div>
+<div id="comments-footer" align="center"><?php echo '<a href="/contacts" title="JComments" target="_blank" rel="nofollow noopener">JComments</a>'; ?></div>
 <?php
 			// Some magic like dynamic comments list loader (anticache) and auto go to anchor script
 			$aca = (int) ($this->getVar('comments-gotocomment') == 1);
